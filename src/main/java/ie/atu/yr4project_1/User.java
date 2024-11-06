1d20931bbb1e4c355d1b6a6f5c0c7cd6bff0317b
ciara <g00402357@atu.ie>
2024-11-06 19:58:34 +0000

    Created User class, with validation. Id will be generated with everything

--- src/main/java/ie/atu/yr4project_1/User.java
+++ src/main/java/ie/atu/yr4project_1/User.java
@@ -1,8 +1,32 @@
 package ie.atu.yr4project_1;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank(message = "Name can't be blank")
+    private String name;
+    @NotBlank(message = "Last name can't be blank")
+    private String lastName;
+    @Email(message = "Must be a valid email")
+    private String email;
+    @Positive(message = "Number must be positive")
+    private String num;
+
 
 
 }
