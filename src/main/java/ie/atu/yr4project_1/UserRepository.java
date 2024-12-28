3726f09000016ffcb0f89c2df2bce423db2cdd94
ciara <g00402357@atu.ie>
2024-12-28 14:05:17 +0000

    fixture for long instead of string

--- src/main/java/ie/atu/yr4project_1/UserRepository.java
+++ src/main/java/ie/atu/yr4project_1/UserRepository.java
@@ -2,5 +2,5 @@ package ie.atu.yr4project_1;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface UserRepository extends MongoRepository<User, String> {
+public interface UserRepository extends MongoRepository<User, Long> {
 }
